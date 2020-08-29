4c6465807d1d5dc64db89f2eaa3b99db77478a98
Furkan Duman <furkan0duman@gmail.com>
2020-08-29 09:13:22 +0300

    Initial Commit

---
+++ yresume/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  yresume
+//
+//  Created by Furkan Duman on 29.08.2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
